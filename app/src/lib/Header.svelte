40eb34bf937a0c83ddd884285bcc461eedfe5f81
¨kemken077¨ <kemken077@gmail.com>
2023-02-27 13:05:54 -0300

    Add main components and texts objects

---
+++ app/src/lib/Header.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let title = 'This is a default title';
+</script>
+
+<main>
+  <h1>{title}</h1>
+</main>
+
+<style>
+  h1 {
+    text-transform: capitalize;
+  }
+</style>
---
+++ app/src/texts/languages.ts
@@ -0,0 +1,11 @@
+export const ENGLISH = {
+  header: {
+    title: 'How much money I have?'
+  }
+};
+
+export const SPANISH = {
+  header: {
+    title: '¿Cuanto dinero tengo?'
+  }
+};
