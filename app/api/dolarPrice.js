3f4df998fe909de30429e46b68edd18617c77992
¨kemken077¨ <kemken077@gmail.com>
2023-02-27 15:42:07 -0300

    Utils functions, API endpoint

--- app/api/dolarPrice.js
+++ app/api/dolarPrice.js
@@ -1,3 +1,18 @@
+import axios from 'axios';
+import cheerio from 'cheerio';
+
+const getCurrencyData = (currencyName, values) => {
+  const currency = {
+    currencyName,
+    values,
+  };
+  return currency;
+};
+
+const parseValue = (value, splitIdentifier) => {
+  return value !== '' ? parseFloat(value.split(splitIdentifier)[1]) : null;
+};
+
 export default function dolarPrice(request, response) {
   const message = 'Hello Serverless functions!';
   console.log(message);
