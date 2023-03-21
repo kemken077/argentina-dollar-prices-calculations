1bfd9e7b3fb5a905b5b705d892dc5fa175fbd6a8
¨kemken077¨ <kemken077@gmail.com>
2023-03-21 14:34:29 -0300

    Decouple main layout from app component

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,42 +1,9 @@
 <script>
-  import Calculations from "./lib/Calculations.svelte";
-  import CurrencyButtons from "./lib/CurrencyButtons.svelte";
-  import DolarPrices from "./lib/DolarPrices.svelte";
-  import Header from "./lib/Header.svelte";
-  import { SPANISH } from './texts/languages';
-  import { calculationMode } from "./stores/stores";
-
-  let selectedMode;
-  calculationMode.subscribe(newStoredMode => {
-    selectedMode = newStoredMode;
-  });
+  import Loader from './lib/Loader.svelte';
 </script>
 
-<main>
-  <Header title={SPANISH.header.title} />
-  <div class="container">
-    <section class="mode-trigger">
-      <CurrencyButtons mode={selectedMode} />
-    </section>
-    <section class="calculations">
-      <Calculations mode={selectedMode} />
-    </section>
-    <section class="prices">
-      <DolarPrices />
-    </section>
-  </div>
-</main>
 
-<style>
-  .container {
-    margin: 0 auto;
-    text-align: center;
-  }
-  section {
-    max-width: 100%;
-    width: 100%;
-  }
-  section.mode-trigger {
-    margin: 20px auto;
-  }
-</style>
+<Loader this={() => import('./lib/MainLayout.svelte')}>
+  <img slot="fallback" src="https://media.tenor.com/sfTuw8H8zUUAAAAC/money-flying.gif" alt="Money flying" />
+</Loader>
+
