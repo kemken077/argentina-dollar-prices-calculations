2621a074f1f4be6f45560a4c533be18a03caaf49
¨kemken077¨ <kemken077@gmail.com>
2023-04-30 23:51:31 -0300

    Use node resolve for Rollup vite import issues

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), nodeResolve()],
   build: {
     rollupOptions: {
       external: [
-        './node_modules/carbon-components-svelte',
+        /node_modules/,
       ],
     },
   },
