f45878a71a804ffacb764c9ea57ab893f978940b
¨kemken077¨ <kemken077@gmail.com>
2023-04-30 23:28:15 -0300

    Deployment fix try

--- app/vite.config.js
+++ app/vite.config.js
@@ -8,8 +8,8 @@ export default defineConfig({
     rollupOptions: {
       external: [
         /node_modules/,
-      '/node_modules/carbon-components-svelte/css/all.css',
-      '/node_modules/carbon-components-svelte']
+      './node_modules/carbon-components-svelte/css/all.css',
+      './node_modules/carbon-components-svelte']
     }
   }
 })
