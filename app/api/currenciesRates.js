d4a32c6acf7f21ee0867c8523fd8499c11bdd49c
¨kemken077¨ <kemken077@gmail.com>
2023-05-01 00:29:36 -0300

    Local allowCors function for API endpoint

--- app/api/currenciesRates.js
+++ app/api/currenciesRates.js
@@ -1,6 +1,21 @@
 import axios from 'axios';
-import { allowCors } from './utils/allowCors';
 
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
 
 function currenciesRates(request, response) {
   const _API_KEY = 'af96sub64uoicr3kvgd8ao7dctbtrrodbq6qrre45ioq8aj2rs2us8'; 
