3b16e60556194a40287eb0ef3afb421b3b157d3c
¨kemken077¨ <kemken077@gmail.com>
2023-04-30 23:57:28 -0300

    Re install carbon and add Rollup config plugin

--- app/vite.config.js
+++ app/vite.config.js
@@ -4,9 +4,10 @@ import { nodeResolve } from '@rollup/plugin-node-resolve'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), nodeResolve()],
+  plugins: [svelte()],
   build: {
     rollupOptions: {
+      plugins: [nodeResolve()],
       external: [
         /node_modules/,
       ],
