edeb53b614b2944d042aae2c26a783e123cce243
¨kemken077¨ <kemken077@gmail.com>
2023-04-30 21:23:48 -0300

    Combo box for currency selection

--- app/src/lib/SelectCurrency.svelte
+++ app/src/lib/SelectCurrency.svelte
@@ -1,36 +1,40 @@
-<!-- <script>
-// @ts-nocheck
-  import { calculationMode } from '../stores/stores';
-  import { Select, SelectItem } from "carbon-components-svelte";
-</script>
-
-<Select
-  labelText="Currency"
-  on:change={(e) => calculationMode.set(e.target.value)}
->
-  <SelectItem value="dollars" />
-  <SelectItem value="pesos" />
-</Select> -->
-
 <script>
   import { ComboBox } from "carbon-components-svelte";
   import { calculationMode } from '../stores/stores';
+  const currencies = [
+    {id: 0, text: 'USD - US Dollar', label: 'dollars'},
+    {id: 1, text: 'ARS - Argentina Peso', label: 'pesos'},
+    {id: 2, text: 'BRL - Brazilian Real', label: 'real'},
+    {id: 3, text: 'EUR - Euro', label: 'euro'},
+    {id: 4, text: 'GBP - British Pound', label: 'pound'},
+    {id: 5, text: 'AUD - Australian Dollar', label: 'ausdollar'},
+    {id: 6, text: 'CAD - Canadian Dollar', label: 'candollar'},
+    {id: 7, text: 'JPY - Japanese Yen', label: 'yen'},
+    {id: 8, text: 'CNY - Chinese Yuan Renminbi', label: 'yuan'},
+  ];
   function handleSelect(event) {
-    const currencyMode = event.detail.selectedItem.text;
-    console.log({event, currency: event.detail.selectedItem.text});
+    const currencyMode = event.detail.selectedItem.label;
+    console.log({event, currencyText: event.detail.selectedItem.text});
+    console.log({event, currencyLabel: event.detail.selectedItem.label});
     calculationMode.set(currencyMode);
+  }
+  /**
+     * @param {number} id
+     * @param {string} text
+     */
+  function getComboItem(id, text) {
+    const item = {
+      id,
+      text,
+    }; 
+    return item;
   }
 </script>
 
 <ComboBox
   titleText=""
   placeholder="Elige una divisa"
-  items={[
-    { id: "0", text: "dollars" },
-    { id: "1", text: "pesos" },
-    { id: "2", text: "euros" },
-    { id: "3", text: "reales" },
-  ]}
+  items={currencies}
   on:select={handleSelect}
 />
 
